00a0ba9a30c0d1be11e9be40bd6c48a0886a2788
VarNotUsed <varnotused@protonmail.com>
2023-06-13 11:24:21 +0200

    initial commit

---
+++ packages/ui/src/components/Button/Button.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
+
+// Default metadata of the story https://storybook.js.org/docs/react/api/csf#default-export
+const meta: Meta<typeof Button> = {
+  title: "Components/Button",
+  component: Button,
+  argTypes: {
+    textColor: { control: "color" },
+    onClick: { action: "clicked" },
+  },
+};
+
+export default meta;
+
+// The story type for the component https://storybook.js.org/docs/react/api/csf#named-story-exports
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    label: "Primary 😃",
+    size: "large",
+    type: "primary",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    ...Primary.args,
+    type: "secondary",
+    label: "Secondary 😇",
+  },
+};
