b3b7614dd4f051f94b73de6dcb118100aac3bf37
Milan Steiner <steiner.milan@hotmail.com>
2023-06-28 11:25:38 +0200

    Fix collapsable

--- packages/ui/src/SidebarNavigation/SidebarNavigation.tsx
+++ packages/ui/src/SidebarNavigation/SidebarNavigation.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import Colors from '../Theme/colors';
-import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import * as React from "react";
+import Colors from "../Theme/colors";
+import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
 import {
   Drawer as MuiDrawer,
   DrawerProps as MuiDrawerProps,
 } from "@mui/material";
-import List from '@mui/material/List';
-import IconButton from '@mui/material/IconButton';
-import ListSubheader from '@mui/material/ListSubheader';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import List from "@mui/material/List";
+import IconButton from "@mui/material/IconButton";
+import ListSubheader from "@mui/material/ListSubheader";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 interface Items {
   label: string;
@@ -27,55 +27,52 @@ const drawerWidth = 240;
 
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: 'flex-start',
+  justifyContent: "flex-start",
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
   }),
-);
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
-const SidebarNavigation = ({
-  items,
-  ...props
-}: DrawerProps) => {
+const SidebarNavigation = ({ items, ...props }: DrawerProps) => {
   const theme = useTheme();
   const [open, setOpen] = React.useState(true);
 
@@ -89,98 +86,96 @@ const SidebarNavigation = ({
 
   return (
     <Drawer
+      PaperProps={{
+        sx: {
+          background: Colors.backgroundSidenav,
+          boxShadow: "-1px 0px 0px 0px rgba(228, 228, 228, 0.10) inset",
+        },
+      }}
+      variant="permanent"
+      open={open}
+      {...props}
+    >
+      <DrawerHeader
         sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-          }
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "30px",
         }}
-        PaperProps={{
-          sx: {
-            background: Colors.backgroundSidenav,
-            boxShadow: '-1px 0px 0px 0px rgba(228, 228, 228, 0.10) inset',
-          }
-        }}
-        variant="permanent"
-        open={open}
-        {...props}
       >
-        <DrawerHeader
+        <img src="/logo.svg" />
+        <IconButton
+          onClick={handleDrawerClose}
           sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            padding: '30px'
+            borderRadius: "8px",
+            background:
+              "linear-gradient(180deg, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0.025) 100%)",
+            transform: theme.direction === "rtl" ? "rotate(180deg)" : "none",
+            padding: "10px",
           }}
         >
-          <img src="/logo.svg"/>
-          <IconButton 
-            onClick={handleDrawerClose}
+          <img src="/arrow.svg" />
+        </IconButton>
+      </DrawerHeader>
+      <List>
+        <ListSubheader
+          sx={{
+            paddingLeft: "40px",
+            background: "unset",
+            fontSize: "14px",
+            lineHeight: "16px",
+            marginBottom: "20px",
+          }}
+        >
+          Menu
+        </ListSubheader>
+        {items.map((item, index) => (
+          <ListItem
+            key={item.label}
+            disablePadding
             sx={{
-              borderRadius: '8px',
-              background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0.025) 100%)',
-              transform: theme.direction === 'rtl' ? 'rotate(180deg)' : 'none',
-              padding: '10px'
+              margin: "0 16px",
+              width: "unset",
+              padding: 0,
+              borderRadius: "12px",
+              overflow: "hidden",
+              border: item.active
+                ? "1px solid #E2491A"
+                : "1px solid transparent",
+              background: item.active
+                ? "rgba(226, 73, 26, 0.10)"
+                : "transparent",
             }}
           >
-            <img src="/arrow.svg"/>
-          </IconButton>
-        </DrawerHeader>
-        <List>
-          <ListSubheader
-            sx={{
-              paddingLeft: '40px',
-              background: 'unset',
-              fontSize: '14px',
-              lineHeight: '16px',
-              marginBottom: '20px'
-            }}
-          >
-            Menu
-          </ListSubheader>
-          {items.map((item, index) => (
-            <ListItem 
-              key={item.label} 
-              disablePadding
+            <ListItemButton
               sx={{
-                margin: '0 16px',
-                width: 'unset',
                 padding: 0,
-                borderRadius: '12px',
-                overflow: 'hidden',
-                border: item.active ? '1px solid #E2491A' : '1px solid transparent',
-                background: item.active ? 'rgba(226, 73, 26, 0.10)' : 'transparent'
               }}
             >
-              <ListItemButton
+              <ListItemIcon
                 sx={{
-                  padding: 0
+                  minWidth: "24px",
+                  marginLeft: "20px",
                 }}
               >
-                <ListItemIcon
-                  sx={{
-                    minWidth: '24px',
-                    marginLeft: '20px'
-                  }}
-                >
-                  {item.icon}
-                </ListItemIcon>
-                <ListItemText
-                  primaryTypographyProps={{
-                    fontSize: '14px',
-                    lineHeight: '20px'
-                  }} 
-                  sx={{
-                    padding: '16px 24px 16px 20px'
-                  }}
-                  primary={item.label} 
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText
+                primaryTypographyProps={{
+                  fontSize: "14px",
+                  lineHeight: "20px",
+                }}
+                sx={{
+                  padding: "16px 24px 16px 20px",
+                }}
+                primary={item.label}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
-}
+};
 
 export { SidebarNavigation };
