ecabc0daf385fc6eaea8af18a2c7e0eedda740c7
Milan Steiner <steiner.milan@hotmail.com>
2023-06-28 13:59:11 +0200

    Adjust margins dashboard price charts

--- packages/ui/src/Dashboard/DashboardCharts/DashboardCharts.tsx
+++ packages/ui/src/Dashboard/DashboardCharts/DashboardCharts.tsx
@@ -17,7 +17,7 @@ const GlowingChart = ({ data }: { data: Data[] }) => (
   <ResponsiveContainer width="100%" height={250}>
     <AreaChart
       data={data}
-      margin={{ top: -10, right: -10, left: -10, bottom: 0 }}
+      margin={{ top: 0, right: -10, left: -10, bottom: 0 }}
     >
       <defs>
         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
