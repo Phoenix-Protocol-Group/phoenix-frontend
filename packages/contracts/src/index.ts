2a5775b1aed1afa7973e4a0be0f7c2020ed7c512
Milan Steiner <steiner.milan@hotmail.com>
2023-09-05 12:06:19 +0200

    Adding Factory contract API

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,3 +1,4 @@
 export * as PhoenixPairContract from "./phoenix-pair";
 export * as SorobanTokenContract from "./soroban-token";
 export * as PhoenixStakeContract from "./phoenix-stake";
+export * as PhoenixFactoryContract from "./phoenix-factory";
