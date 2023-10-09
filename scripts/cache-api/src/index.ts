28e6bea8f67cc4d46e883cb939b466718f00b1ac
VarNotUsed <varnotused@protonmail.com>
2023-10-09 18:11:31 +0200

    prisma schema relations

--- scripts/cache-api/src/index.ts
+++ scripts/cache-api/src/index.ts
@@ -9,14 +9,88 @@ const port = process.env.PORT || 3000;
 
 fetch.startFetch();
 
+async function mockData() {
+  const tokenA = await prisma.token.create({
+    data: {
+      address: "CC5BDQ7J2VK4TQHHIMFNVMV5ZJZYDXDZN7XQ7IM73XKKPYF2KKARCOIW"
+    },
+  });
+
+  const tokenB = await prisma.token.create({
+    data: {
+      address: "CC6HQVYSKVFCKWU6EKYDILHFOV5DC26VICEUYAMKTIDS4XZPYMDP3WOS"
+    },
+  });
+
+  const tokenShare = await prisma.token.create({
+    data: {
+      address: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ"
+    },
+  });
+
+  const pairTokenA = await prisma.pairToken.create({
+    data: {
+      amount: 10000,
+      token: {
+        connect: {
+          id: tokenA.id,
+        },
+      },
+    },
+  });
+
+  const pairTokenB = await prisma.pairToken.create({
+    data: {
+      amount: 10000,
+      token: {
+        connect: {
+          id: tokenB.id,
+        },
+      },
+    },
+  });
+
+  const pairTokenShare = await prisma.pairToken.create({
+    data: {
+      amount: 10000,
+      token: {
+        connect: {
+          id: tokenShare.id,
+        },
+      },
+    },
+  });
+
+  const pair = await prisma.pair.create({
+    data: {
+      address: "paircontractaddress",
+      assetA: {
+        connect: {
+          id: pairTokenA.id,
+        },
+      },
+      assetB: {
+        connect: {
+          id: pairTokenB.id,
+        },
+      },
+      assetShare: {
+        connect: {
+          id: pairTokenShare.id,
+        },
+      }
+    },
+  });
+
+  const pairs = await prisma.pair.findMany();
+  console.log(pairs);
+}
+
+//mockData();
+
 const resolvers = {
   Query: {
-    allPairs: () => {
-      return prisma.pair.findMany();
-    },
-    allTokens: () => {
-      return prisma.token.findMany();
-    }
+    getPairs: prisma.pair.findMany(),
   }
 };
 
