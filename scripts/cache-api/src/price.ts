4c6dd07e0507c8d4698e492a36377c5754a97e9f
VarNotUsed <varnotused@protonmail.com>
2023-10-17 20:01:45 +0200

    allow multiple targets

--- scripts/cache-api/src/price.ts
+++ scripts/cache-api/src/price.ts
@@ -6,43 +6,57 @@ interface Graph {
   [key: string]: string[];
 }
 
-function findBestPath(pairArray: Pair[], stringArray: string[]): string[] | null {
-  const graph: Graph = {};
+function findBestPath(
+  pair: Pair,
+  pairArray: Pair[],
+  targetArray: string[]
+): { [key: string]: { [key: string]: string[] } } {
+  const result: { [key: string]: { [key: string]: string[] } } = {};
 
-  for (const p of pairArray) {
-    const [address1, address2] = p;
-    if (!graph[address1]) {
-      graph[address1] = [];
-    }
-    if (!graph[address2]) {
-      graph[address2] = [];
-    }
-    graph[address1].push(address2);
-    graph[address2].push(address1);
-  }
+  const getPath = (start: string, targets: string[]): { [key: string]: string[] } => {
+    const graph: Graph = {};
 
-  const queue: { node: string; path: string[] }[] = pairArray.map(([a, b]) => ({
-    node: a,
-    path: [a],
-  }));
-
-  const visited: { [key: string]: boolean } = {};
-
-  while (queue.length > 0) {
-    const { node, path } = queue.shift()!;
-    if (stringArray.includes(node)) {
-      return path;
+    for (const p of pairArray) {
+      const [address1, address2] = p;
+      if (!graph[address1]) {
+        graph[address1] = [];
+      }
+      if (!graph[address2]) {
+        graph[address2] = [];
+      }
+      graph[address1].push(address2);
+      graph[address2].push(address1);
     }
-    if (!graph[node] || visited[node]) {
-      continue;
-    }
-    visited[node] = true;
-    for (const neighbor of graph[node]) {
-      queue.push({ node: neighbor, path: [...path, neighbor] });
-    }
-  }
 
-  return null;
+    const resultPaths: { [key: string]: string[] } = {};
+    const visited: { [key: string]: boolean } = {};
+
+    for (const target of targets) {
+      const queue: { node: string; path: string[] }[] = [{ node: start, path: [start] }];
+
+      while (queue.length > 0) {
+        const { node, path } = queue.shift()!;
+        if (node === target) {
+          resultPaths[target] = path;
+          break;
+        }
+        if (!graph[node] || visited[node]) {
+          continue;
+        }
+        visited[node] = true;
+        for (const neighbor of graph[node]) {
+          queue.push({ node: neighbor, path: [...path, neighbor] });
+        }
+      }
+    }
+
+    return resultPaths;
+  };
+
+  result[pair[0]] = getPath(pair[0], targetArray);
+  result[pair[1]] = getPath(pair[1], targetArray);
+
+  return result;
 }
 
 const coinGecko = new CoinGeckoClient({
