1e335b1ecc0dbfad760c82047c69ee2e4d4802e4
VarNotUsed <varnotused@protonmail.com>
2023-10-17 19:40:10 +0200

    findpaths rewrite

--- scripts/cache-api/src/price.ts
+++ scripts/cache-api/src/price.ts
@@ -1,5 +1,55 @@
-import { findBestPath } from "./demoFindPaths";
 import { CoinGeckoClient } from 'coingecko-api-v3';
+
+type Pair = [string, string];
+
+interface Graph {
+  [key: string]: string[];
+}
+
+function findBestPath(
+  pair: Pair,
+  pairArray: Pair[],
+  stringArray: string[]
+): string[] | null {
+  const graph: Graph = {};
+
+  for (const p of pairArray) {
+    const [address1, address2] = p;
+    if (!graph[address1]) {
+      graph[address1] = [];
+    }
+    if (!graph[address2]) {
+      graph[address2] = [];
+    }
+    graph[address1].push(address2);
+    graph[address2].push(address1);
+  }
+
+  const visited: { [key: string]: boolean } = {};
+  const path: string[] = [];
+
+  const dfs = (node: string, target: string, path: string[]): string[] | null => {
+    if (node === target) {
+      return path;
+    }
+    if (!graph[node] || visited[node]) {
+      return null;
+    }
+    visited[node] = true;
+    for (const neighbor of graph[node]) {
+      const result = dfs(neighbor, target, [...path, neighbor]);
+      if (result) {
+        return result;
+      }
+    }
+    return null;
+  };
+
+  const [source, target] = pair;
+  const result = dfs(source, target, [source]);
+  return result;
+}
+
 
 const coinGecko = new CoinGeckoClient({
   timeout: 10000,
