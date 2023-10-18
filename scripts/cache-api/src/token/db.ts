d95da3783c510cc9e450069ceaaa19f084dbf31c
VarNotUsed <varnotused@protonmail.com>
2023-10-18 17:24:36 +0200

    added isSharedToken attribute to model

--- scripts/cache-api/src/token/db.ts
+++ scripts/cache-api/src/token/db.ts
@@ -2,6 +2,16 @@ import prisma from "../prisma";
 
 export async function getAll() {
   const tokens = await prisma.token.findMany();
+  
+  return tokens;
+}
+
+export async function getAllExceptShared() {
+  const tokens = await prisma.token.findMany({
+    where: {
+      isSharedToken: false,
+    }
+  });
 
   return tokens;
 }
@@ -16,14 +26,15 @@ export async function getByAddress(address: string) {
   return token;
 }
 
-export async function getOrCreate(address: string) {
+export async function getOrCreate(address: string, isSharedToken: boolean) {
   const token = await getByAddress(address);
 
   if(token !== null) return token;
 
   const newToken = await prisma.token.create({
     data: {
-      address: address
+      address: address,
+      isSharedToken: isSharedToken,
     },
   });
 
