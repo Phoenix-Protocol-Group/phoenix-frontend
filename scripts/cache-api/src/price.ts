7d6750b347dc8773644b56be381d589b1f9a0792
VarNotUsed <varnotused@protonmail.com>
2023-10-18 20:25:15 +0200

    calculate ratio

--- scripts/cache-api/src/price.ts
+++ scripts/cache-api/src/price.ts
@@ -12,40 +12,74 @@ interface PathNode {
   token: string;
 }
 
-export function findBestPath(tokenSymbol: string, pairsArray: Pair[], targets: string[]): PathNode[] {
-  const graph: { [key: string]: { [key: string]: number } } = {};
-  const pairIdMap: { [key: string]: number } = {};
+export function findBestPath(tokenSymbol: string, pairsArray: Pair[], targets: string[]): { ratio: number, path: Pair[] } {
+  const graph: { [key: string]: { [key: string]: Pair } } = {};
   pairsArray.forEach((item) => {
     const tokenA = item.tokenA;
     const tokenB = item.tokenB;
     if (!graph[tokenA]) graph[tokenA] = {};
     if (!graph[tokenB]) graph[tokenB] = {};
-    graph[tokenA][tokenB] = item.id;
-    graph[tokenB][tokenA] = item.id;
-    pairIdMap[`${tokenA}-${tokenB}`] = item.id;
-    pairIdMap[`${tokenB}-${tokenA}`] = item.id;
+    graph[tokenA][tokenB] = item;
+    graph[tokenB][tokenA] = item;
   });
 
   const visited: Set<string> = new Set();
-  const queue: { node: string; path: PathNode[] }[] = [{ node: tokenSymbol, path: [] }];
-  const paths: PathNode[] = [];
+  const queue: { node: string; path: Pair[], ratio: number }[] = [{ node: tokenSymbol, path: [], ratio: 1 }];
+  let finalRatio = 1;
 
   while (queue.length > 0) {
-    const { node, path } = queue.shift()!;
+    const { node, path, ratio } = queue.shift()!;
     if (targets.includes(node)) {
-      const pairId = pairIdMap[`${tokenSymbol}-${node}`];
-      if (pairId !== undefined) {
-        paths.push({ id: pairId, token: node });
-      }
+      finalRatio = ratio;
+      return { ratio: finalRatio, path };
     }
     if (!visited.has(node)) {
       visited.add(node);
       for (const neighbor in graph[node]) {
-        queue.push({ node: neighbor, path: [...path, { id: graph[node][neighbor], token: neighbor }] });
+        let nextRatio = ratio;
+        if (graph[node][neighbor].tokenB === node) {
+          nextRatio *= graph[node][neighbor].ratio;
+        } else {
+          nextRatio *= 1 / graph[node][neighbor].ratio;
+        }
+        queue.push({ node: neighbor, path: [...path, graph[node][neighbor]], ratio: nextRatio });
       }
     }
   }
-  return paths;
+  return { ratio: finalRatio, path: [] };
+}
+
+export function calculateTokenValue(tokenSymbol: string, targetTokens: string[], pairsArray: Pair[], targetArray: string[]): number {
+  let value = 0;
+  let found = false;
+  for (const targetToken of targetTokens) {
+    const { ratio, path } = findBestPath(tokenSymbol, pairsArray, targetArray);
+    if (path.length > 0) {
+      for (let i = 0; i < path.length; i++) {
+        if (path[i].tokenA === tokenSymbol && path[i].tokenB === targetToken) {
+          if (path[i].ratio > 0) {
+            value = 1 * path[i].ratio;
+          } else {
+            value = 1 / -path[i].ratio;
+          }
+          found = true;
+          break;
+        } else if (path[i].tokenB === tokenSymbol && path[i].tokenA === targetToken) {
+          if (path[i].ratio > 0) {
+            value = 1 / path[i].ratio;
+          } else {
+            value = 1 * -path[i].ratio;
+          }
+          found = true;
+          break;
+        }
+      }
+    }
+    if (found) {
+      break;
+    }
+  }
+  return value;
 }
 
 const coinGecko = new CoinGeckoClient({
@@ -53,7 +87,7 @@ const coinGecko = new CoinGeckoClient({
   autoRetry: true,
 });
 
-export async function getPrice(ids: string, currency?: string) {
+export async function getPrices(ids: string, currency?: string) {
   const prices = await coinGecko.simplePrice({
     ids: ids,
     vs_currencies: currency || "usd",
