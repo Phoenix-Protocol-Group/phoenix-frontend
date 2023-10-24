2dfe714def791fe74bd14d67a4cd195a8a1a6981
Milan Steiner <steiner.milan@hotmail.com>
2023-10-24 16:44:08 +0200

    Fix secondary button

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -23,42 +23,44 @@ const Button = ({
   ...props
 }: ButtonProps) => {
   const styles = {
-    background: type == "primary" ? Colors.primary : Colors.background,
-    border: type == "primary" ? "none" : "1px solid",
-    borderImage:
-      "linear-gradient(180deg, #E2391B 0%, #E29E1B 100%),linear-gradient(0deg, #000000, #000000)",
-    padding: size == "medium" ? "18px 40px 18px 40px" : "12px 40px 12px 40px",
-    borderRadius: "16px",
-    fontSize: "14px",
-    fontWeight: "700",
-    lineHeight: "20px",
-    textTransform: "none",
-    color: Colors.backgroundLight,
-    "&:hover": {
-      background:
-        "linear-gradient(137deg, #E2491A 0%, #E21B1B 17.08%, #E2491A 42.71%, #E2AA1B 100%)",
+    wrapper: {
+      display: "inline-block",
+      background: "linear-gradient(180deg, #E2391B 0%, #E29E1B 100%)",
+      padding: "1px", // This is the border witdh
+      borderRadius: "16px",
     },
-    "&:disabled": {
-      background:
-        "background: linear-gradient(137deg, #E2491A 0%, #E21B1B 17.08%, #E2491A 42.71%, #E2AA1B 100%)",
-      opacity: 0.3,
+    button: {
+      background: type === "primary" ? Colors.primary : Colors.background,
+      border: "none",
+      padding:
+        size === "medium" ? "18px 40px 18px 40px" : "12px 40px 12px 40px",
+      borderRadius: "15px", // Slighter less then the wrapper to show the border
+      fontSize: "14px",
+      fontWeight: "700",
+      lineHeight: "20px",
+      textTransform: "none",
+      color: Colors.backgroundLight,
+      width: "100%",
     },
   };
+
   const { sx, ...otherProps } = props;
 
   return (
-    <MuiButton
-      size={size}
-      disabled={props.disabled}
-      //  @ts-ignore
-      sx={{
-        ...styles,
-        ...props.sx,
-      }}
-      {...otherProps}
-    >
-      {props.children || label}
-    </MuiButton>
+    <div style={type === "secondary" ? styles.wrapper : {}}>
+      <MuiButton
+        size={size}
+        disabled={props.disabled}
+        //  @ts-ignore
+        sx={{
+          ...styles.button,
+          ...props.sx,
+        }}
+        {...otherProps}
+      >
+        {props.children || label}
+      </MuiButton>
+    </div>
   );
 };
 
