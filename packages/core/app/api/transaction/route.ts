069fbd7716f684ed1651c2823f1fa416f999ba90
Milan Steiner <steiner.milan@hotmail.com>
2024-01-11 14:26:56 +0100

    Remove debugs

--- packages/core/app/api/transaction/route.ts
+++ packages/core/app/api/transaction/route.ts
@@ -17,19 +17,14 @@ export async function POST(req: NextRequest) {
         }
       );
     }
-    console.log(1);
-
     const networkPassphrase: string = "Test SDF Network ; September 2015";
     const tx = new Transaction(transaction, networkPassphrase);
-    console.log(2);
     for (const op of tx.operations as Operation[]) {
       if (op.type === "manageData" && op.name === "client_domain") {
-        console.log(process.env.ANCHOR_PRIVATE_KEY);
         const anchorPrivateKey = process.env.ANCHOR_PRIVATE_KEY!;
         tx.sign(Keypair.fromSecret(anchorPrivateKey));
       }
     }
-    console.log(3);
     return new NextResponse(JSON.stringify({ signedTransaction: tx.toXDR() }), {
       status: 200,
       headers: {
