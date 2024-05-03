b6dab6f49f7f9618a4667dc8b4a88035a37113ac
Milan Steiner <steiner.milan@hotmail.com>
2024-05-03 13:40:37 +0200

    Enable countdown once again

--- packages/core/app/layout.tsx
+++ packages/core/app/layout.tsx
@@ -206,7 +206,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <style>{css}</style>
 
           {/* Pre-Launch */}
-          {false ? (
+          {true ? (
             <Countdown />
           ) : (
             <>
