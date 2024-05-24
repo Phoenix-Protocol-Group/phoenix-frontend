f4c8e647ec0ec8ba80c4d457210c52b7d25e7b0e
Milan Steiner <steiner.milan@hotmail.com>
2024-05-24 16:49:21 +0200

    Remove unused imports in @phoenix-protocol/contracts

--- packages/contracts/src/soroban-token/index.ts
+++ packages/contracts/src/soroban-token/index.ts
@@ -1,19 +1,19 @@
-import {Buffer} from "buffer";
-import type {i128, u32,} from '@stellar/stellar-sdk/contract';
+import { Buffer } from "buffer";
+import type { i128, u32 } from "@stellar/stellar-sdk/contract";
 import {
-    AssembledTransaction,
-    Client as ContractClient,
-    ClientOptions as ContractClientOptions,
-    Spec as ContractSpec,
-} from '@stellar/stellar-sdk/contract';
+  AssembledTransaction,
+  Client as ContractClient,
+  ClientOptions as ContractClientOptions,
+  Spec as ContractSpec,
+} from "@stellar/stellar-sdk/contract";
 
-export * from '@stellar/stellar-sdk'
-export * as contract from '@stellar/stellar-sdk/contract'
-export * as rpc from '@stellar/stellar-sdk/rpc'
+export * from "@stellar/stellar-sdk";
+export * as contract from "@stellar/stellar-sdk/contract";
+export * as rpc from "@stellar/stellar-sdk/rpc";
 
-if (typeof window !== 'undefined') {
-    //@ts-ignore Buffer exists
-    window.Buffer = window.Buffer || Buffer;
+if (typeof window !== "undefined") {
+  //@ts-ignore Buffer exists
+  window.Buffer = window.Buffer || Buffer;
 }
 
 export const networks = {
