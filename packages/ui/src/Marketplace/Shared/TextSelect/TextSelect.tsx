cfe3d83e2bcf80029a04095b7037c11d7aaa2a4c
VarNotUsed <varnotused@protonmail.com>
2024-06-10 15:36:59 +0200

    textselect component

---
+++ packages/ui/src/Marketplace/Shared/TextSelect/TextSelect.tsx
@@ -0,0 +1,77 @@
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+
+export interface TextSelectItemProps {
+  label: string;
+  value: string;
+}
+
+export interface TextSelectProps {
+  label: string;
+  helpText?: string;
+  value: string;
+  onChange: (value: string) => void;
+  items: TextSelectItemProps[];
+}
+
+const TextSelect = (props: TextSelectProps) => {
+  return (
+    <>
+      <Typography
+        sx={{
+          fontSize: "12px",
+          lineHeight: "17px",
+          mb: 1.5,
+          color: "#BFBFBF",
+        }}
+      >
+        {props.label}{" "}
+        {props.helpText && (
+          <Tooltip title={props.helpText}>
+            <HelpOutlineIcon
+              sx={{
+                ml: 1,
+                fontSize: "16px",
+                position: "relative",
+                top: 3,
+                "&:hover": { color: "#FFF" },
+              }}
+            />
+          </Tooltip>
+        )}
+      </Typography>
+      <FormControl
+        sx={{
+          minWidth: 120,
+          width: "100%",
+          "& .MuiSelect-outlined": {
+            padding: "14px 8px 14px 16px !important",
+          },
+        }}
+      >
+        <Select
+          value={props.value}
+          onChange={(e) => props.onChange(e.target.value)}
+          displayEmpty
+          inputProps={{ "aria-label": "Without label" }}
+          sx={{ borderRadius: "16px", opacity: 0.4, fontSize: "14px" }}
+        >
+          {props.items.map((item: TextSelectItemProps, index: number) => (
+            <MenuItem key={index} value={item.value}>
+              {item.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </>
+  );
+};
+
+export default TextSelect;
--- packages/ui/src/Marketplace/Shared/index.ts
+++ packages/ui/src/Marketplace/Shared/index.ts
@@ -2,5 +2,14 @@ import BackButton from "./BackButton/BackButton";
 import CreateOptionCard from "./CreateOptionCard/CreateOptionCard";
 import ImageUpload from "./ImageUpload/ImageUpload";
 import TextInput from "./TextInput/TextInput";
+import TextArea from "./TextArea/TextArea";
+import TextSelect from "./TextSelect/TextSelect";
 
-export { BackButton, CreateOptionCard, ImageUpload, TextInput };
+export {
+  BackButton,
+  CreateOptionCard,
+  ImageUpload,
+  TextInput,
+  TextArea,
+  TextSelect,
+};
