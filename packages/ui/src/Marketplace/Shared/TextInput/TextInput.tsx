9089be63091be47d5d1b759659b574e61e81b1b8
VarNotUsed <varnotused@protonmail.com>
2024-06-10 15:13:03 +0200

    Input Component

---
+++ packages/ui/src/Marketplace/Shared/TextInput/TextInput.tsx
@@ -0,0 +1,85 @@
+import { TextField, Tooltip, Typography } from "@mui/material";
+import React from "react";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+
+export interface TextInputProps {
+  label: string;
+  helpText?: string;
+  placeholder: string;
+  value: string;
+  onChange: (val: string) => void;
+}
+
+const TextInput = (props: TextInputProps) => {
+  return (
+    <>
+      <Typography
+        sx={{
+          fontSize: "12px",
+          lineHeight: "17px",
+          mb: 1.5,
+          color: "#BFBFBF",
+        }}
+      >
+        {props.label}{" "}
+        {props.helpText && (
+          <Tooltip title={props.helpText}>
+            <HelpOutlineIcon
+              sx={{
+                ml: 2,
+                fontSize: "16px",
+                position: "relative",
+                top: 3,
+                "&:hover": { color: "#FFF" },
+              }}
+            />
+          </Tooltip>
+        )}
+      </Typography>
+      <TextField
+        id="search"
+        type="search"
+        value={props.value}
+        placeholder="Search"
+        sx={{
+          color: "white",
+          width: "100%",
+          "&::placeholder": {
+            fontSize: "14px",
+            color: "white",
+            opacity: 0.4,
+          },
+          "& .MuiInputBase-input": {
+            padding: "14px 8px 14px 16px !important",
+          },
+        }}
+        onChange={(e) => props.onChange(e.target.value)}
+        InputLabelProps={{
+          sx: {
+            color: "white!important",
+            fontSize: "0.8125rem",
+            opacity: 0.6,
+            textAlign: "center",
+          },
+        }}
+        InputProps={{
+          sx: {
+            color: "white",
+            fontSize: "14px",
+            opacity: 0.6,
+            borderRadius: "16px",
+            "&:hover fieldset": {
+              border: "1px solid #E2621B!important",
+            },
+            "&:focus-within fieldset, &:focus-visible fieldset": {
+              border: "2px solid #E2621B!important",
+              color: "white!important",
+            },
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default TextInput;
--- packages/ui/src/Marketplace/Shared/index.ts
+++ packages/ui/src/Marketplace/Shared/index.ts
@@ -1,5 +1,6 @@
 import BackButton from "./BackButton/BackButton";
 import CreateOptionCard from "./CreateOptionCard/CreateOptionCard";
 import ImageUpload from "./ImageUpload/ImageUpload";
+import TextInput from "./TextInput/TextInput";
 
-export { BackButton, CreateOptionCard, ImageUpload };
+export { BackButton, CreateOptionCard, ImageUpload, TextInput };
