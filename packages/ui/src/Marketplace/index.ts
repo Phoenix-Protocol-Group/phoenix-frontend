d67511f1f643650d0ca47eb0b59619fd773ecfa3
VarNotUsed <varnotused@protonmail.com>
2024-06-26 17:26:31 +0200

    added missing imports

--- packages/ui/src/Marketplace/index.ts
+++ packages/ui/src/Marketplace/index.ts
@@ -1,9 +1,19 @@
 import Frontpage from "./Frontpage";
 import * as Shared from "./Shared";
 import CreateCollection from "./CreateCollection/CreateCollection";
+import CollectionsOverview from './CollectionsOverview/CollectionsOverview';
+import CollectionSingle from './CollectionSingle/CollectionSingle';
+import NftSingle from './NftSingle/NftSingle';
+import CreateNftBulk from './CreateNftBulk/CreateNftBulk';
+import CreateNft from './CreateNft/CreateNft';
 
 export {
   Frontpage,
   Shared,
-  CreateCollection
+  CreateCollection,
+  CollectionsOverview,
+  CollectionSingle,
+  NftSingle,
+  CreateNftBulk,
+  CreateNft
 };
