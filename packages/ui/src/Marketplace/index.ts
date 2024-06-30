d9d006962329ca86f25b40bf72bfbc9db5278571
VarNotUsed <varnotused@protonmail.com>
2024-06-30 19:20:19 +0200

    nft single import

--- packages/ui/src/Marketplace/index.ts
+++ packages/ui/src/Marketplace/index.ts
@@ -3,9 +3,9 @@ import * as Shared from "./Shared";
 import CreateCollection from "./CreateCollection/CreateCollection";
 import CollectionsOverview from './CollectionsOverview/CollectionsOverview';
 import CollectionSingle from './CollectionSingle/CollectionSingle';
-import NftSingle from './NftSingle/NftSingle';
 import CreateNftBulk from './CreateNftBulk/CreateNftBulk';
 import CreateNft from './CreateNft/CreateNft';
+import NftSingle from "./NftSingle/NftSingle";
 
 export {
   Frontpage,
