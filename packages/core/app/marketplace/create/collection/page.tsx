03a09d3ada933055fe4db0dfa8e2157db085cae7
VarNotUsed <varnotused@protonmail.com>
2024-07-18 13:24:56 +0200

    change title of create pages

--- packages/core/app/marketplace/create/collection/page.tsx
+++ packages/core/app/marketplace/create/collection/page.tsx
@@ -46,7 +46,7 @@ export default function Page() {
           mb: 6,
         }}
       >
-        Marketplace
+        Create
       </Typography>
       <Box mx={2}>
         <CreateCollection
--- packages/core/app/marketplace/create/page.tsx
+++ packages/core/app/marketplace/create/page.tsx
@@ -49,7 +49,7 @@ export default function Page() {
           mb: 6,
         }}
       >
-        Marketplace
+        Create
       </Typography>
       <Box mx={2} position="relative">
         <CreateSomething
