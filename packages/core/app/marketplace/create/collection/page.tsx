4ed2972f6aca3c34476e30ea0199cf0e7b1443cd
VarNotUsed <varnotused@protonmail.com>
2024-09-12 14:45:07 +0200

    connect collection creation ui to contract

--- packages/core/app/marketplace/create/collection/page.tsx
+++ packages/core/app/marketplace/create/collection/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { Box, Typography } from "@mui/material";
+import { PhoenixNFTCollectionDeployerContract } from "@phoenix-protocol/contracts";
 import { TextSelectItemProps } from "@phoenix-protocol/types";
+import { constants, Signer } from "@phoenix-protocol/utils";
 import { CreateCollection } from "@phoenix-protocol/ui";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useAppStore, usePersistStore } from "@phoenix-protocol/state";
 
 export default function Page() {
+  const store = useAppStore();
+  const storePersist = usePersistStore();
   const router = useRouter();
 
   const [name, setName] = useState<string>("");
@@ -20,14 +25,56 @@ export default function Page() {
   );
 
   useEffect(() => {
-    if(!file) return;
+    if (!file) return;
 
     const url = URL.createObjectURL(file);
     setPreviewImage(url);
   }, [file]);
 
-  const handleSubmitClick = () => {
+  const handleSubmitClick = async () => {
+    if (!storePersist) return;
 
+    const randomBytes = new Uint8Array(32);
+    const random = crypto.getRandomValues(randomBytes);
+
+    const salt = Buffer.from(random);
+
+    try {
+      const signer =
+        storePersist.wallet.walletType === "wallet-connect"
+          ? store.walletConnectInstance
+          : new Signer();
+
+      const CollectionDeployerContract =
+        new PhoenixNFTCollectionDeployerContract.Client({
+          publicKey: storePersist.wallet.address!,
+          contractId:
+            "CDH7YFXQFAHAFIPH64SC77WWD2RTII3XK6SVESRUHFBAHFWFJYNMKKSY", //@todo move to constants
+          networkPassphrase: constants.NETWORK_PASSPHRASE,
+          rpcUrl: constants.RPC_URL,
+          // @ts-ignore
+          signTransaction: (tx: string) =>
+            storePersist.wallet.walletType === "wallet-connect"
+              ? signer.signTransaction(tx)
+              : // @ts-ignore
+                signer.sign(tx),
+        });
+
+      const tx = await CollectionDeployerContract.deploy_new_collection({
+        salt,
+        admin: storePersist.wallet.address!,
+        name,
+        symbol,
+      });
+
+      const res = await tx?.signAndSend();
+
+      if(res.result) {
+        router.push(`/marketplace/collections/${res.result}`)
+      }
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
