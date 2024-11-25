b2a938b42c32d19b24f0f0a4bfab425842a663ff
Milan Steiner <steiner.milan@hotmail.com>
2024-11-25 21:41:31 +0100

    Fix previous arrowception

--- packages/ui/src/Transactions/TransactionsTable/TransactionEntry.tsx
+++ packages/ui/src/Transactions/TransactionsTable/TransactionEntry.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, useMediaQuery } from "@mui/material";
 import { ArrowForward, ArrowBack } from "@mui/icons-material";
 import { TransactionTableEntryProps } from "@phoenix-protocol/types";
 
@@ -14,6 +14,8 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
     boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)",
   };
 
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
   return (
     <Box sx={{ ...BoxStyle, mt: 2 }}>
       <Box
@@ -23,8 +25,8 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
         sx={{
           position: "absolute",
           top: "50%",
-          left: -40,
-          width: "20%",
+          left: isMobile ? -20 : -40,
+          width: isMobile ? "12%" : "20%",
           height: "auto",
           opacity: 0.1,
           transform: "translateY(-50%)",
@@ -38,8 +40,8 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
         sx={{
           position: "absolute",
           top: "50%",
-          right: -40,
-          width: "20%",
+          right: isMobile ? -20 : -40,
+          width: isMobile ? "12%" : "20%",
           height: "auto",
           opacity: 0.1,
           transform: "translateY(-50%)",
@@ -49,10 +51,10 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
       <Grid
         container
         alignItems="center"
-        spacing={3}
+        spacing={isMobile ? 1 : 3}
         sx={{ position: "relative", zIndex: 2 }}
       >
-        <Grid item xs={2}>
+        <Grid item xs={isMobile ? 3 : 2}>
           <Box
             sx={{
               borderRadius: "16px",
@@ -66,13 +68,13 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
                   : "rgba(76, 175, 80, 0.2)",
               color:
                 props.type.toLowerCase() === "sell" ? "#FF5722" : "#4CAF50",
-              fontSize: "14px",
+              fontSize: isMobile ? "12px" : "14px",
               py: 0.5,
               px: 1.5,
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
-              width: "96px",
+              width: isMobile ? "72px" : "96px",
               fontWeight: "bold",
               textTransform: "uppercase",
             }}
@@ -81,7 +83,7 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
           </Box>
         </Grid>
 
-        <Grid item xs={4}>
+        <Grid item xs={isMobile ? 5 : 4}>
           <Box
             sx={{
               display: "flex",
@@ -90,7 +92,7 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
               gap: "8px",
             }}
           >
-            {props.type.toLowerCase() === "sell" ? (
+            {props.type.toLowerCase() == "sell" ? (
               <>
                 <Box
                   sx={{
@@ -102,12 +104,12 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
                     component="img"
                     src={props.assets[0].icon}
                     alt={props.assets[0].name}
-                    sx={{ width: "24px", height: "24px", mr: "0.5rem" }}
+                    sx={{ width: "20px", height: "20px", mr: "0.5rem" }}
                   />
                   <Typography
                     sx={{
                       color: "#FFF",
-                      fontSize: "16px",
+                      fontSize: isMobile ? "14px" : "16px",
                       fontWeight: "500",
                     }}
                   >
@@ -115,7 +117,11 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
                   </Typography>
                 </Box>
                 <ArrowForward
-                  sx={{ fontSize: "16px", color: "#FF5722", mx: 1 }}
+                  sx={{
+                    fontSize: isMobile ? "14px" : "16px",
+                    color: "#FF5722",
+                    mx: 1,
+                  }}
                 />
                 <Box
                   sx={{
@@ -127,12 +133,12 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
                     component="img"
                     src={props.assets[1].icon}
                     alt={props.assets[1].name}
-                    sx={{ width: "24px", height: "24px", mr: "0.5rem" }}
+                    sx={{ width: "20px", height: "20px", mr: "0.5rem" }}
                   />
                   <Typography
                     sx={{
                       color: "#FFF",
-                      fontSize: "16px",
+                      fontSize: isMobile ? "14px" : "16px",
                       fontWeight: "500",
                     }}
                   >
@@ -152,19 +158,25 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
                     component="img"
                     src={props.assets[0].icon}
                     alt={props.assets[0].name}
-                    sx={{ width: "24px", height: "24px", mr: "0.5rem" }}
+                    sx={{ width: "20px", height: "20px", mr: "0.5rem" }}
                   />
                   <Typography
                     sx={{
                       color: "#FFF",
-                      fontSize: "16px",
+                      fontSize: isMobile ? "14px" : "16px",
                       fontWeight: "500",
                     }}
                   >
                     {props.assets[0].name}
                   </Typography>
                 </Box>
-                <ArrowBack sx={{ fontSize: "16px", color: "#4CAF50", mx: 1 }} />
+                <ArrowBack
+                  sx={{
+                    fontSize: isMobile ? "14px" : "16px",
+                    color: "#4CAF50",
+                    mx: 1,
+                  }}
+                />
                 <Box
                   sx={{
                     display: "flex",
@@ -175,12 +187,12 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
                     component="img"
                     src={props.assets[1].icon}
                     alt={props.assets[1].name}
-                    sx={{ width: "24px", height: "24px", mr: "0.5rem" }}
+                    sx={{ width: "20px", height: "20px", mr: "0.5rem" }}
                   />
                   <Typography
                     sx={{
                       color: "#FFF",
-                      fontSize: "16px",
+                      fontSize: isMobile ? "14px" : "16px",
                       fontWeight: "500",
                     }}
                   >
@@ -192,12 +204,12 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
           </Box>
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item xs={isMobile ? 2 : 2}>
           <Typography
             sx={{
               color:
                 props.type.toLowerCase() === "sell" ? "#FF5722" : "#4CAF50",
-              fontSize: "16px",
+              fontSize: isMobile ? "14px" : "16px",
               fontWeight: "600",
             }}
           >
@@ -205,11 +217,11 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
           </Typography>
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item xs={isMobile ? 2 : 2}>
           <Typography
             sx={{
               color: "#B0E0E6",
-              fontSize: "14px",
+              fontSize: isMobile ? "12px" : "14px",
               fontWeight: "400",
             }}
           >
@@ -217,11 +229,11 @@ const TransactionEntry = (props: TransactionTableEntryProps) => {
           </Typography>
         </Grid>
 
-        <Grid item xs={2}>
+        <Grid item xs={isMobile ? 12 : 2}>
           <Typography
             sx={{
               color: "#FFF",
-              fontSize: "14px",
+              fontSize: isMobile ? "12px" : "14px",
               opacity: "0.7",
             }}
           >
