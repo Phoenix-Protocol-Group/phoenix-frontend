63c3c2f216749e2dee03468c2da98fe1cf9322dd
Milan Steiner <steiner.milan@hotmail.com>
2024-12-01 13:04:33 +0100

    Set access allow origin

--- packages/core/app/api/stellar/route.ts
+++ packages/core/app/api/stellar/route.ts
@@ -1,15 +1,26 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export async function GET() {
-  const filePath = path.join(process.cwd(), './stellar.toml');
-  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const filePath = path.join(process.cwd(), "./stellar.toml");
 
-  return new Response(fileContents, {
-    status: 200,
-    headers: {
-      'Content-Type': 'text/plain',
-      'Access-Control-Allow-Origin': '*',
-    },
-  });
+  try {
+    const fileContents = fs.readFileSync(filePath, "utf8");
+
+    return new Response(fileContents, {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain",
+        "Access-Control-Allow-Origin": "*", // Allow all origins
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+      },
+    });
+  } catch (error) {
+    return new Response("Error reading stellar.toml", {
+      status: 500,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
 }
