b76865b45f39b08334b64c49f2e4cc30aba7c0cd
VarNotUsed <varnotused@protonmail.com>
2024-12-18 10:02:59 +0100

    state constants on mainnet

--- packages/state/src/constants.ts
+++ packages/state/src/constants.ts
@@ -1,14 +1,14 @@
 /**
  * The Soroban network passphrase used to initialize this library.
  */
-export const NETWORK_PASSPHRASE: string = "Test SDF Network ; September 2015";
+export const NETWORK_PASSPHRASE: string = "Public Global Stellar Network ; September 2015";
 
 /**
  * The Soroban RPC endpoint used to initialize this library.
  */
-export const RPC_URL: string = "https://soroban-testnet.stellar.org";
+export const RPC_URL: string = "https://mainnet.stellar.validationcloud.io/v1/YcyPYotN_b6-_656rpr0CabDwlGgkT42NCzPVIqcZh0";
 
 /**
  * The Soroban NETWORK used to initialize this library.
  */
-export const NETWORK: string = "TESTNET";
+export const NETWORK: string = "STANDALONE";
