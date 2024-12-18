d9436c113414f4ac72ea415afcc02aa1bc830023
VarNotUsed <varnotused@protonmail.com>
2024-12-18 10:53:47 +0100

    constants for testnet

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -9,13 +9,13 @@ export const TESTING_SOURCE: Account = new Account(
  * The Soroban network passphrase used to initialize this library.
  */
 export const NETWORK_PASSPHRASE: string =
-  "Public Global Stellar Network ; September 2015";
+  "Test SDF Network ; September 2015";
 
 /**
  * The Soroban RPC endpoint used to initialize this library.
  */
 export const RPC_URL: string =
-  "https://mainnet.stellar.validationcloud.io/v1/YcyPYotN_b6-_656rpr0CabDwlGgkT42NCzPVIqcZh0";
+  "https://soroban-testnet.stellar.org";
 
 /**
  * Factory contract address
